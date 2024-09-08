b46e13a8893eaf8e28726f340ad8ff92e44e115d
Joanne Greenhalgh <jo.fbs.uk@gmail.com>
2024-09-08 20:10:19 +0100

    fix: updated setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 from os import walk, path
 
-URL = "https://github.com/OpenVoiceOS/skill-ovos-hello-world"
+URL = "https://github.com/skutterbob/ovos-skill-greenhome"
 SKILL_CLAZZ = "GreenhomeSkill"  # needs to match __init__.py class name
 PYPI_NAME = "ovos-skill-greenhome"  # pip install PYPI_NAME
 
