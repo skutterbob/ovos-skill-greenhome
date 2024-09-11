eff6be14e8fe293c56d5c69878549d573360bdc3
Joanne Greenhalgh <jo.fbs.uk@gmail.com>
2024-09-11 20:14:33 +0100

    Test url

--- __init__.py
+++ __init__.py
@@ -117,7 +117,8 @@ class GreenhomeSkill(OVOSSkill):
         # submit password
         self.speak_dialog("radio.controls")
         self.gui.clear()
-        self.gui.show_url(self, self.vlc_url, override_idle=None, override_animations=False)
+        self.gui.show_url(self, "https://www.bbc.com/sport", override_idle=None, override_animations=False)
+        #self.gui.show_url(self, self.vlc_url, override_idle=None, override_animations=False)
         
         LOG.info("There are five types of log messages: 'info, debug, warning, error, and exception.")
         # Skills can log useful information. These will appear in the CLI and
